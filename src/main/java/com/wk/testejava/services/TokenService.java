2cb61b5863c8324dbd990bb2f5e54d559f38a653
Bryan William <bryanwac@gmail.com>
2023-06-18 19:39:51 -0300

    fix: timezone + token expiration

--- src/main/java/com/wk/testejava/services/TokenService.java
+++ src/main/java/com/wk/testejava/services/TokenService.java
@@ -40,7 +40,7 @@ public class TokenService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration((new Date(System.currentTimeMillis() + 1000 * 60 * 24))) // duração do token
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // expiração de uma hora
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
